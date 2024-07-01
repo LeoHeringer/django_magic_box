62949601e672253ff3f42c1c6ff37965fec194a3
Leonardo Heringer <69641220+LeoHeringer@users.noreply.github.com>
2024-07-01 01:18:22 -0300

    urls

--- magic/urls.py
+++ magic/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from .views import create_product, create_box, create_subscription, create_shipment, create_review, create_promotion
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('create_product/', create_product, name='create_product'),
+    path('create_box/', create_box, name='create_box'),
+    path('create_subscription/', create_subscription, name='create_subscription'),
+    path('create_shipment/', create_shipment, name='create_shipment'),
+    path('create_review/', create_review, name='create_review'),
+    path('create_promotion/', create_promotion, name='create_promotion'),
 ]
